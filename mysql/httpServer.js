816b3e7db5a57ee1d65e3ce8069fff943a402785
admin <admin>
2017-05-11 16:25:53 +0800

    Signed-off-by: admin <admin>

--- mysql/httpServer.js
+++ mysql/httpServer.js
@@ -54,8 +54,8 @@ var selectAllTables = function (pool,res) {
       res.end('查询失败!' + 'err');
   });
 }
-// 查询数据表下数据 10条记录
-var selectTables = function (pool,res,table,page) {
+// 查询数据表下数据 10条记录 json 显示
+var selectTablesJson = function (pool,res,table,page) {
   var n = 10;
   LIMIT1 = (page-1)*n;
   sql = 'select * from '+table+' LIMIT '+LIMIT1+','+n;
@@ -95,6 +95,66 @@ var selectTables = function (pool,res,table,page) {
       res.end('查询失败!' + 'err');
   });
 }
+// 查询数据表下数据 10条记录 表格显示
+var selectTablesTable = function (pool,res,table,page) {
+  var n = 10;
+  LIMIT1 = (page-1)*n;
+  var lPage = page - 1;
+  var rPage = page + 1;
+  sql = 'select * from '+table+' LIMIT '+LIMIT1+','+n;
+  // log.error(sql);
+  _mysql.query(pool,sql)
+  .then(function (data) {
+      // log.error(data.length);
+      var s = '<table style="word-break:break-all;border-collapse: collapse;border: 1px solid #eee;"><tbody>';
+      var _data = [];
+      res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+      if(typeof data !== 'object'){
+        res.end('查询失败!');
+      }else{
+        //返回值 组成新数组
+        // data.forEach(function (value, index, array) {
+        //   if(value.TABLE_SCHEMA == config.database){
+        //     _data.push(value);
+        //   }
+        // });
+        // 修改原始数组
+        // _data.map(function (value, index, array) {
+        //   return value.TABLE_NAME;
+        // });
+        // data = _data;
+        s += '<tr style="padding:10px;border: 1px solid #eee;background: #eee;">';
+        var r = /([a-z_]{1,})(?=:)/g;
+        var jstring = JSON.stringify(data[0]);
+        jstring = jstring.replace(/\"/g,'');
+        // var key = r.exec(jstring);
+        var key = jstring.match(r);
+        log.error(jstring);
+        for (var i = 0;i < key.length; i++) {
+          s += '<td width="100px;" style="padding:10px;border: 1px solid #eee;word-break:normal;">'+key[i]+'</td>';
+        }
+        s += '<td width="100px;" style="padding:10px;border: 1px solid #eee;word-break:normal;"><a href="./?sql=table&table='+table+'&page='+lPage+'">上一页</a> / <a href="./?sql=table&table='+table+'&page='+rPage+'">下一页</a> 操作</td>';
+        s += '</tr>';
+        for(var j = 0; j < data.length; j++){
+            s += '<tr>';
+                for (var k = 0;k < key.length; k++) {
+                  s += '<td style="padding:10px;border: 1px solid #eee;">'+data[j][key[k]]+'</td>';
+                  // s += '<td style="padding:10px;border: 1px solid #eee;"><a href="./?sql=table&table='+table+'&page='+lPage+'">删除</a></td>';
+                }
+            // s += '<td>'+JSON.stringify(data[j])+'</td>';
+            s += '</tr>';
+        }
+        s += '</tbody></table>';
+        res.end(s);
+      }
+      return false;
+  })
+  .catch(function(err){
+      log.error(err);
+      res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+      res.end('查询失败!' + 'err');
+  });
+}
 
 // 创建服务器 查询  t = 数据表
 http.createServer(function (req,res) {
@@ -107,7 +167,7 @@ http.createServer(function (req,res) {
     if(query.sql != 'table'){
         selectAllTables(pool,res);
     }else if(query.table != ''){
-        selectTables(pool,res,query.table,query.page);
+        selectTablesTable(pool,res,query.table,Number(query.page));
     }else{
       res.end();
     }
