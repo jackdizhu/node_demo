8bc445f8cac103924194c6fe4c5a4f90513830e8
admin <admin>
2017-05-16 18:17:21 +0800

    Signed-off-by: admin <admin>

--- 爬虫/request.js
+++ 爬虫/request.js
@@ -2,10 +2,10 @@
 
     const http = require('http'),
           fs = require('fs'),
-          $ = require('cheerio'),
-          req = require('request'),
+          cheerio = require('cheerio'),
+          request = require('request'),
           log = require('./logger.js');
-    var U = 'http://www.baidu.com';
+    var U = 'http://www.gooofashion.com/index.php?app=goods_list&type=new&list1=1';
     http.get(U, function (res) {
         // 页面数据
         var html = '';
@@ -16,7 +16,15 @@
             html += data;
         })
         .on('end', function () {
+            var $ = cheerio.load(html);
+            // 打印 页面 内容
             log.debug(html);
+            var imgSrc = $('img')[0].attribs.src;
+            // 保存图片资源
+            log.debug(imgSrc);
+            if(imgSrc){
+              request(imgSrc).pipe(fs.createWriteStream('./'+ 'test.png'));
+            }
         })
         .on('error', function (err) {
             console.log(err);
