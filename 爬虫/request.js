d53b2a135fa9c6ec0c33cab7ec42efc0e84addf0
admin <admin>
2017-05-16 19:05:01 +0800

    爬取 cnblogs 链接

--- 爬虫/request.js
+++ 爬虫/request.js
@@ -5,7 +5,8 @@
           cheerio = require('cheerio'),
           getSrc = require('request'),
           log = require('./logger.js');
-    var U = 'http://www.th7.cn/d/file/p/2016/10/15/e5ade4d729e5005b18862b8b609c5fbe.jpg';
+    var U = 'http://www.cnblogs.com/post/prevnext?postId=6049237&blogId=239339&dateCreated=2016%2F11%2F10+0%3A25%3A00';
+    var U2 = [];
     http.get(U, function (res) {
         // 页面数据
         var html = '';
@@ -18,13 +19,21 @@
         .on('end', function () {
             var $ = cheerio.load(html);
             // 打印 页面 内容
-            log.debug(html);
-            var imgSrc = $('img')[0].attribs.src;
+            var imgSrc,name,pageU = [];
+            if($('img').length){
+              imgSrc = $('img')[0].attribs.src;
+            }
+            // var a = $('#post_next_prev a').children().length;
+            var a = $('.p_n_p_prefix');
+            for (var i = 0; i < a.length; i++) {
+              pageU.push(a[i].attribs.href);
+            }
+            log.debug(pageU);
             // 保存图片资源
             log.debug(imgSrc);
             var r = /([^/]+.[a-z]+)$/g;
-            var name = imgSrc.match(r);
             if(imgSrc){
+              name = imgSrc.match(r);
               getSrc(imgSrc).pipe(fs.createWriteStream('./'+ name[0]));
             }
         })
