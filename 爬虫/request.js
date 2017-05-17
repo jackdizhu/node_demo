b2ed1b1cdce56bfdd57bb25fb34925eaa8377794
admin <admin>
2017-05-17 10:14:50 +0800

    增加 github 搜索 保存 相关链接功能

--- 爬虫/request.js
+++ 爬虫/request.js
@@ -1,46 +1,20 @@
 (function () {
 
-    const http = require('http'),
+    const http2 = require('./http2.js'),
           fs = require('fs'),
           cheerio = require('cheerio'),
           getSrc = require('request'),
           log = require('./logger.js');
-    var U = 'http://www.cnblogs.com/post/prevnext?postId=6049237&blogId=239339&dateCreated=2016%2F11%2F10+0%3A25%3A00';
-    var U2 = [];
-    http.get(U, function (res) {
-        // 页面数据
-        var html = '';
-        var titles = [];
-        res.setEncoding('utf-8');
-        res
-        .on('data', function (data) {
-            html += data;
-        })
-        .on('end', function () {
-            var $ = cheerio.load(html);
-            // 打印 页面 内容
-            var imgSrc,name,pageU = [];
-            if($('img').length){
-              imgSrc = $('img')[0].attribs.src;
-            }
-            // var a = $('#post_next_prev a').children().length;
-            var a = $('.p_n_p_prefix');
-            for (var i = 0; i < a.length; i++) {
-              pageU.push(a[i].attribs.href);
-            }
-            log.debug(pageU);
-            // 保存图片资源
-            log.debug(imgSrc);
-            var r = /([^/]+.[a-z]+)$/g;
-            if(imgSrc){
-              name = imgSrc.match(r);
-              getSrc(imgSrc).pipe(fs.createWriteStream('./'+ name[0]));
-            }
-        })
-        .on('error', function (err) {
-            console.log(err);
-        });
+    var httpsO = {
+        // U: 'http://www.cnblogs.com/post/prevnext?postId=6049237&blogId=239339&dateCreated=2016%2F11%2F10+0%3A25%3A00',
+        U: 'https://nodejs.org/api/https.html#https_https_get_options_callback',
+        // _http2: 'http'
+        _http2: 'https'
+    };
+    http2(httpsO).then(function (data) {
+      log.debug(data);
     });
 
+    // getSrc(imgSrc).pipe(fs.createWriteStream('./'+ name[0]));  保存图片代码
 
 }());
